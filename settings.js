8c115d088f1476832a340fa7aabaa4ca3c585399
RIFQI BOTZ <88196162+RIFQIBOTZ@users.noreply.github.com>
2022-06-29 19:00:38 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ global.APIs = {
 
 // Free apikey
 global.APIKeys = {
-	'https://api.zeeoneofc.xyz': 'EhPiJ5AN',  // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
+	'https://api.zeeoneofc.xyz': 'BCs552ML',  // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
 }
 
 // setting 
